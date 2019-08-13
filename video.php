6d84952294e2042008f05a0ca530e7dfab1b239d
osuntee <osuntee@gmail.com>
2019-08-13 23:38:31 +0100

    Update video.php

--- video.php
+++ video.php
@@ -11,7 +11,7 @@ function getYoutubeVideoID($url){
 }
             $video_url = $_POST['come'];
 
-$api_key = 'Your_API_KEY';
+$api_key = 'AIzaSyCPsRFxWB2f2ZKQA54HmHvIlTW0qf4TI98';
 
 $api_url = 'https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&maxResults=100&moderationStatus=published&textFormat=plainText&videoId='.getYoutubeVideoID($video_url).'&key='.$api_key; 
 
